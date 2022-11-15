c7d13721764f4552c3b2539a82712bd1873b6800
Alan Jiang <adj2424@gmail.com>
2022-11-15 14:36:52 -0500

    finished chat

--- client/src/pages/ChatRoom.jsx
+++ client/src/pages/ChatRoom.jsx
@@ -4,65 +4,89 @@ const ChatRoom = (props) => {
   const [userName, setUsername] = useState('');
   const [roomId, setRoomId] = useState('');
   const [message, setMessage] = useState('');
+  const [allMessages, setAllMessages] = useState([]);
+  const [joinedRoom, setJoinedRoom] = useState(false);
   const socket = props.socket;
+
   const joinRoom = () => {
     if (userName !== '' && roomId !== '') {
       //pass roomId to backend socket
-      console.log(`userName: ${userName}, roomId: ${roomId}`);
+      console.log(`joined room userName: ${userName}, roomId: ${roomId}`);
       socket.emit('joinRoom', roomId);
+      setJoinedRoom(true);
     }
   };
+
   //async to wait for message to be sent
   const sendMessage = async () => {
     if (message !== '') {
       const msgData = {
         room: roomId,
-        userName: userName,
-        message: message,
+        user: userName,
+        msg: message,
         time: new Date(Date.now()).getHours() + ':' + new Date(Date.now()).getMinutes()
       };
       console.log('sending', msgData);
-      await socket.emit('sendMessage', JSON.stringify(msgData));
+      await socket.emit('sendMessage', msgData);
+      //add message to message list
+      setAllMessages((prev) => [...prev, msgData]);
+      //empties text box
+      setMessage('');
     }
   };
 
   useEffect(() => {
     //listening for new messages
     socket.on('receivedMessage', (msgData) => {
-      console.log('receivedMessage', msgData);
+      console.log('received message', msgData);
+      setAllMessages((prev) => [...prev, msgData]);
     });
   }, [socket]);
 
   return (
     <div id="chatRoomPage">
       <p>Chat Room Page Here</p>
-      <input
-        type="text"
-        placeholder="name"
-        onChange={(e) => {
-          setUsername(e.target.value);
-        }}
-      />
-      <input
-        type="text"
-        placeholder="room id"
-        onChange={(e) => {
-          setRoomId(e.target.value);
-        }}
-      />
-      <button onClick={joinRoom}>Join room</button>
-      <h1>Chat </h1>
-      <div className="header"></div>
-      <div className="body"></div>
-      <div className="footer">
-        <input
-          type="text"
-          placeholder="send message"
-          onChange={(e) => {
-            setMessage(e.target.value);
-          }}></input>
-        <button onClick={sendMessage}>&#9658;</button>
-      </div>
+      {!joinedRoom ? (
+        <div className="joinContainer">
+          <input
+            type="text"
+            placeholder="name"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="room id"
+            onChange={(e) => {
+              setRoomId(e.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join room</button>
+        </div>
+      ) : (
+        <div className="chatContainer">
+          <div className="header">
+            <h1>Chat </h1>
+            <p>Room: {roomId}</p>
+          </div>
+          <div className="body">
+            {allMessages.map((message, index) => {
+              return <p key={index}>{`${message.user}: ${message.msg}`}</p>;
+            })}
+          </div>
+          <div className="footer">
+            <input
+              type="text"
+              value={message}
+              placeholder="send message"
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}></input>
+            <button onClick={sendMessage}>&#9658;</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
