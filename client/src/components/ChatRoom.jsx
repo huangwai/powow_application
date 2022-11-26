ab360940d948504c7e01e917bd622687ffc9eacf
adj2424 <adj2424@gmail.com>
2022-11-25 23:18:42 -0500

    polished chat room and css

--- client/src/components/ChatRoom.jsx
+++ client/src/components/ChatRoom.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import SendIcon from '@mui/icons-material/Send';
+import Box from '@mui/material/Box';
+import '../css/components/ChatRoom.css';
+
 export const ChatRoom = props => {
   const [message, setMessage] = useState('');
   const [allMessages, setAllMessages] = useState([]);
@@ -58,29 +73,69 @@ export const ChatRoom = props => {
 
   return (
     <div id="chatRoomPage">
+      <Box className="header" sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton size="large" color="inherit">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+              Room: {props.roomId}
+            </Typography>
+            <Button color="inherit" startIcon={<PeopleAltIcon />} />
+            <Button color="inherit" onClick={clearMessage} startIcon={<DeleteIcon />} />
+          </Toolbar>
+        </AppBar>
+      </Box>
       <div className="chatContainer">
-        <div className="header">
-          <h1>Chat </h1>
-          <p>
-            Room: {props.roomId} User: {props.userName}
-          </p>
-          <button onClick={clearMessage}>Clear Message</button>
-        </div>
         <div className="body">
-          {allMessages.map((message, index) => {
-            return <p key={index}>{`${message.user}: ${message.message} - ${message.time}`}</p>;
-          })}
+          <Stack direction="column" spacing={0.3}>
+            {allMessages.map((message, index) => {
+              if (message.user === props.userName) {
+                return (
+                  <Box
+                    key={index}
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'flex-end'
+                    }}
+                  >
+                    <Chip sx={{ fontSize: 16, p: 1 }} color="primary" label={`${message.message}`} />
+                  </Box>
+                );
+              } else {
+                return (
+                  <div key={index}>
+                    <p className="messageHeader">{`${message.user} ${message.time}`}</p>
+                    <Chip sx={{ fontSize: 16, p: 1 }} variant="outlined" label={`${message.message}`} />
+                  </div>
+                );
+              }
+            })}
+          </Stack>
         </div>
         <div className="footer">
-          <input
-            type="text"
-            value={message}
-            placeholder="send message"
-            onChange={e => {
-              setMessage(e.target.value);
-            }}
-          ></input>
-          <button onClick={sendMessage}>&#9658;</button>
+          <Box sx={{ display: 'flex' }}>
+            <TextField
+              sx={{ flexGrow: 1 }}
+              className="messageField"
+              size="small"
+              value={message}
+              variant="outlined"
+              label="Send Message"
+              onChange={e => {
+                setMessage(e.target.value);
+              }}
+              onKeyPress={e => {
+                if (e.key === 'Enter') {
+                  sendMessage(message);
+                }
+              }}
+            />
+            <Button variant="outlined" onClick={sendMessage} endIcon={<SendIcon />}>
+              Send
+            </Button>
+          </Box>
         </div>
       </div>
     </div>
