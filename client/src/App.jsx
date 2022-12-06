28ea0c36fa6eeea397a07408b6042350b2b3d470
adj2424 <adj2424@gmail.com>
2022-12-06 03:42:29 -0500

    worked chat page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,13 +7,11 @@ import CreateRoom from './pages/CreateRoom';
 import { Room } from './pages/Room';
 import About from './pages/About';
 import NoPage from './pages/NoPage';
-import { MenuBar } from './components/MenuBar';
 import ContactForm from './components/ContactForm';
 import io from 'socket.io-client';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import GridTemplateColumns from './pages/ChatOptions';
-import Chat from '@mui/icons-material/Chat';
 import Footer from './components/Footer';
 import ComplexGrid from './pages/ChatOptions2';
 import Header from './components/Header';
@@ -37,7 +35,7 @@ function App() {
             <Route exact path="/createRoom" element={<CreateRoom socket={socket} />} />
             <Route exact path="/joinRoom" element={<JoinRoom socket={socket} />} />
             <Route exact path="/about" element={<About />} />
-            <Route exact path="/chatOptions" element={<GridTemplateColumns />} />
+            <Route exact path="/chatOptions" element={<ComplexGrid />} />
             <Route exact path="/contact" element={<ContactForm />} />
             <Route exact path="/publicChat" element={<PublicChat />} />
             <Route exact path="/room/:id" element={<Room socket={socket} userName={'temp'} />} />
